638808e8d3db7df19f1b0e50f2dd370a4492837f
Murilo Solino <103014786+murilosolino@users.noreply.github.com>
2024-10-15 20:04:53 -0300

    Add files via upload
    Adição da classe Cliente e Interface operacaoReserva, utilizada por Cliente e Hotel.

---
+++ src/models/Cliente.java
@@ -0,0 +1,106 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Cliente implements operacaoReserva{
+    private String nome;
+    private String cpf;
+    private String email;
+    private Date dt_nascimento;
+    private String telefone;
+    private String endereco;
+    private List<String> observacoes;
+
+    Cliente(){}
+
+    public Cliente(String nome, String cpf, String email, Date dt_nascimento, String telefone, String endereco, List<String> observacoes) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.dt_nascimento = dt_nascimento;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.observacoes = observacoes;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDt_nascimento() {
+        return dt_nascimento;
+    }
+
+    public void setDt_nascimento(Date dt_nascimento) {
+        this.dt_nascimento = dt_nascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+
+
+    public List<Object> historicoCliente(){
+        List<Object> historico = new ArrayList<>();
+        return historico;
+    }
+
+    @Override
+    public void fazerReserva() {
+
+    }
+
+    public void cancelarReserva(){
+
+    }
+
+    @Override
+    public List<Object> verificarReserva() {
+        return List.of();
+    }
+
+    public void atualizarDados(){
+
+    }
+
+
+
+
+
+
+}
---
+++ src/models/operacaoReserva.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.List;
+
+public interface operacaoReserva {
+
+    public void fazerReserva();
+
+    public List<Object> verificarReserva();
+
+}
