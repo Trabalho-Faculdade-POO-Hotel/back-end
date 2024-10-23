332b331e9de2f7b9e82f8dd17b0946a14c3fc94e
Murilo Solino <103014786+murilosolino@users.noreply.github.com>
2024-10-22 21:10:03 -0300

    ajuste implementcao de metodo

--- src/main/java/br/com/projeto/app/impl/ClienteDAOImpl.java
+++ src/main/java/br/com/projeto/app/impl/ClienteDAOImpl.java
@@ -32,28 +32,5 @@ public class ClienteDAOImpl implements ClienteDAO {
     @Override
     public void atualizarDadosCadastro(Cliente cliente, String nome, String email, String telefone, String endereco, Date dt_nascimento) {
 
-        try{
-
-            if (cliente.getNome() != null && !cliente.getNome().isEmpty()) {
-                cliente.setNome(nome);
-            }
-            if (cliente.getEmail() != null && !cliente.getEmail().isEmpty()) {
-                cliente.setEmail(email);
-            }
-            if (cliente.getTelefone()!= null && !cliente.getTelefone().isEmpty()) {
-                cliente.setTelefone(telefone);
-            }
-            if (cliente.getEndereco() != null && !cliente.getEndereco().isEmpty()) {
-                cliente.setTelefone(endereco);
-            }
-            if (cliente.getDtNascimento() != null) {
-                cliente.setDtNascimento(dt_nascimento);
-            }
-
-        } catch (Exception e){
-            e.printStackTrace();
-            System.err.println("Cliente não foi cadastrado ou faltam dados no cadastro" +
-                    "Entrar em contato com o Hotel");
-        }
     }
 }
