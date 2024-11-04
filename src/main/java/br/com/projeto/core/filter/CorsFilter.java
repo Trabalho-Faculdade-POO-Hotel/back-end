88c49df7f5dac62d5a10efa947f178905cbe979d
renatex314 <renatocorte34@gmail.com>
2024-11-04 00:43:28 -0300

    CORS ¬_¬ - 04/11/2024 - Renato Corte

---
+++ src/main/java/br/com/projeto/core/filter/CorsFilter.java
@@ -0,0 +1,33 @@
+package br.com.projeto.core.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class CorsFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletResponse httpResp = (HttpServletResponse) response;
+        httpResp.setHeader("Access-Control-Allow-Origin", "*");
+        httpResp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        httpResp.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
